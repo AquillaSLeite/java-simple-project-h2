40a6c02d33a4d46df4901b1021b9dc4e3c59ea13
AquillaLeite <aquillaleite@msigroup.com.br>
2020-10-14 15:14:49 -0300

    start project

---
+++ src/test/java/com/simpleprojecth2/SimpleProjectH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleprojecth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleProjectH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
